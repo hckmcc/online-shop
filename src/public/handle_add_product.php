df71888ecb9ba28fd8e6f4559563f54b2733412a
hckmcc <hckmcc@gmail.com>
2024-10-08 15:07:06 +0300

    tryes OOP style

--- src/public/handle_add_product.php
+++ src/public/handle_add_product.php
@@ -6,8 +6,21 @@ if(!isset($_SESSION))
 if (!isset($_SESSION['user_id'])){
     header('Location: /login');
 }else{
-    $user_id = $_SESSION['user_id'];
     $product_id = $_POST["product_id"];
+    if(!empty($product_id)) {
+        $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        $stmt = $pdo->prepare("SELECT * FROM products WHERE id = :product_id");
+        $stmt->execute(['product_id' => $product_id]);
+        $result = $stmt->fetchAll();
+        if (!$result){
+            echo "Invalid request";
+            die;
+        }
+    }else{
+        echo "Invalid request";
+        die;
+    }
+    $user_id = $_SESSION['user_id'];
     if (empty($_POST["amount"])){
         $amount=1;
     }else{
--- src/public/index.php
+++ src/public/index.php
@@ -1,24 +1,29 @@
 <?php
-
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-if ($requestUri==='/login'){
-    $pageArr=['post'=>'./handle_login.php','get'=>'./get_login.php'];
-    getPage($pageArr, $requestMethod);
-}elseif ($requestUri==='/register'){
-    $pageArr=['post'=>'./handle_registration.php','get'=>'./get_registration.php'];
-    getPage($pageArr, $requestMethod);
-}elseif ($requestUri==='/catalog'){
-    $pageArr=['post'=>null,'get'=>'./catalog.php'];
-    getPage($pageArr, $requestMethod);
-}elseif($requestUri==='/add_product'){
-    $pageArr=['post'=>'./handle_add_product.php','get'=>null];
-    getPage($pageArr, $requestMethod);
-}elseif($requestUri==='/cart'){
-    $pageArr=['post'=>null,'get'=>'./cart.php'];
-    getPage($pageArr, $requestMethod);
-} else{
-    http_response_code(404);
+switch ($requestUri){
+    case '/login':
+        $pageArr=['post'=>'./handle_login.php','get'=>'./get_login.php'];
+        getPage($pageArr, $requestMethod);
+        break;
+    case '/register':
+        $pageArr=['post'=>'./handle_registration.php','get'=>'./get_registration.php'];
+        getPage($pageArr, $requestMethod);
+        break;
+    case '/catalog':
+        $pageArr=['post'=>null,'get'=>'./catalog.php'];
+        getPage($pageArr, $requestMethod);
+        break;
+    case '/add_product':
+        $pageArr=['post'=>'./handle_add_product.php','get'=>null];
+        getPage($pageArr, $requestMethod);
+        break;
+    case '/cart':
+        $pageArr=['post'=>null,'get'=>'./cart.php'];
+        getPage($pageArr, $requestMethod);
+        break;
+    default:
+        http_response_code(404);
 }
 
 function getPage ($pageArr, $requestMethod){
